ea20425c8c6680b7e200e368cf5a0a7e318918cf
Anus Khalil <anuskhalil77@gmail.com>
2023-07-31 10:53:32 +0500

    some-code changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $('#update_user').submit(function(event){
     console.log(data);
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `https://helpful-puce-windbreaker.cyclic.cloud/api/users/${data.id}`,
         "method": "PUT",
         "data": data
     }
@@ -32,7 +32,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `https://helpful-puce-windbreaker.cyclic.cloud/api/users/${id}`,
             "method": "DELETE"
         }
 
