6625ec8972a5d81eafc9f07fec18adbf10d64e71
Anus Khalil <anuskhalil77@gmail.com>
2023-08-02 15:50:45 +0500

    FEAK: Some Code Changes

--- server/services/render.js
+++ server/services/render.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 exports.homeRoutes = (req, res) => {
 
     //Make a get request to /api/users
-    axios.get('https://helpful-puce-windbreaker.cyclic.cloud/api/users')
+    axios.get('https://tiny-erin-llama-hat.cyclic.cloud/api/users')
     .then(function(response){
         res.render('index', {users : response.data});
     })  
@@ -18,7 +18,7 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) => {
-    axios.get('https://helpful-puce-windbreaker.cyclic.cloud/api/users', {params : {id:req.query.id}})
+    axios.get('https://tiny-erin-llama-hat.cyclic.cloud/api/users', {params : {id:req.query.id}})
     .then(function(userdata){
         res.render("update_user", {user : userdata.data})
     })
